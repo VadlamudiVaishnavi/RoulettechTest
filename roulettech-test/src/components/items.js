e3cce550e34a2570a969289549bbe9ac260f8763
VadlamudiVaishnavi <vaishv@bu.edu>
2024-07-18 01:02:25 -0500

    "Helper function fetches images."

--- roulettech-test/src/components/items.js
+++ roulettech-test/src/components/items.js
@@ -1,25 +1,23 @@
-import kungPaoChickenImg from '../images/kung-pao-chicken.jpg';
-import sweetAndSourPorkImg from '../images/sweet-and-sour-pork.jpg';
-import friedRiceImg from '../images/fried-rice.jpg';
+import images from '../helpers/importImages';
 
 const items = [
   {
     title: 'Kung Pao Chicken',
     description: 'Ingredients: Chicken breast, peanuts, bell peppers, soy sauce, garlic, ginger.',
     instructions: 'Instructions: Stir-fry chicken until browned, add vegetables and peanuts, then pour in sauce mixture and cook until thickened.',
-    image: kungPaoChickenImg
+    image: images['kung-pao-chicken.jpg']
   },
   {
     title: 'Sweet and Sour Pork',
     description: 'Ingredients: Pork loin, pineapple, bell peppers, soy sauce, vinegar, sugar.',
     instructions: 'Instructions: Deep fry pork until golden, then stir-fry with vegetables and pineapple. Add sauce mixture and simmer until sauce thickens.',
-    image: sweetAndSourPorkImg
+    image: images['sweet-and-sour-pork.jpg']
   },
   {
     title: 'Fried Rice',
     description: 'Ingredients: Rice, eggs, peas, carrots, soy sauce, green onions.',
     instructions: 'Instructions: Stir-fry vegetables, scramble eggs, then add rice and soy sauce. Stir in green onions and cook until heated through.',
-    image: friedRiceImg
+    image: images['fried-rice.jpg']
   },
 ];
 
