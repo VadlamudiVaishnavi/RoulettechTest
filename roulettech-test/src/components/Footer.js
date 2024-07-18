38994415e9dc3c78b083780928c47cb8bb1c6c4e
VadlamudiVaishnavi <vaishv@bu.edu>
2024-07-18 01:19:17 -0500

    "Renamed header and footer."

--- roulettech-test/src/components/Footer.js
+++ roulettech-test/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="App-footer">
-      <p>2024 Roulettech Test, Round 1?.</p>
+      <p>Roulettech Test, Round 1 - Screening round</p>
     </footer>
   );
 }
--- roulettech-test/src/components/Header.js
+++ roulettech-test/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <header className="App-header">
-      <h1>Roulettech Test, Task 1</h1>
+      <h1>Cookbook</h1>
     </header>
   );
 }
